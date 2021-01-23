edc668d7576887efdc5f340d1414541921847494
Evseev Oleg <oleg063053@gmail.com>
2021-01-23 14:18:29 +0400

    9.2. Checkstyle [#456325]

--- src/ru/job4j/condition/Point.java
+++ src/ru/job4j/condition/Point.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Point {
     public static double distance(int x1, int y1, int x2, int y2) {
-        return Math.sqrt(Math.pow(x2-x1,2) + Math.pow(y2-y1,2));
+        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
     }
 
     public static void main(String[] args) {
@@ -14,6 +14,6 @@ public class Point {
         int x2 = scanner.nextInt();
         int y2 = scanner.nextInt();
 //      double result = Point.distance(x1, y1, x2, y2);
-        System.out.println("result (" + x1 + ", " + y1 + ") to (" + x2 +", " + y2 +") " + Point.distance(x1, y1, x2, y2));
+        System.out.println("result (" + x1 + ", " + y1 + ") to (" + x2 + ", " + y2 + ") " + Point.distance(x1, y1, x2, y2));
     }
 }
--- src/ru/job4j/condition/SqArea.java
+++ src/ru/job4j/condition/SqArea.java
@@ -2,10 +2,11 @@ package ru.job4j.condition;
 
 public class SqArea {
     public static double square(int p, int k) {
-        double height =(double) p / (2 * (k + 1));
+        double height = (double) p / (2 * (k + 1));
         double length = height * k;
         return height * length;
     }
+
     public static void main(String[] args) {
         double result1 = SqArea.square(4, 1);
         System.out.println(" p = 4, k = 1, s = 1, real = " + result1);
