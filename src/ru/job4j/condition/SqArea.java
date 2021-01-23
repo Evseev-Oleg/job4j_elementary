a636300c2bebeadfde42a298659aef392b22c459
Evseev Oleg <oleg063053@gmail.com>
2021-01-23 12:45:56 +0400

    8. Стороны прямоугольника. [#456312] дубль два)

--- src/ru/job4j/condition/SqArea.java
+++ src/ru/job4j/condition/SqArea.java
@@ -2,9 +2,10 @@ package ru.job4j.condition;
 
 public class SqArea {
     public static double square(int p, int k) {
-        return (double) (p/(2*(k+1))*k)*p/(2*(k+1));
+        double height =(double) p / (2 * (k + 1));
+        double length = height * k;
+        return height * length;
     }
-
     public static void main(String[] args) {
         double result1 = SqArea.square(4, 1);
         System.out.println(" p = 4, k = 1, s = 1, real = " + result1);
