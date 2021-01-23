6ea6b02b69b76654094a325764b4af573ecdb95d
Evseev Oleg <oleg063053@gmail.com>
2021-01-23 17:40:39 +0400

    3. Тесты для определения расстояния между точками. [#456344]

--- test/ru/job4j/condition/PointTest.java
+++ test/ru/job4j/condition/PointTest.java
@@ -8,12 +8,34 @@ import static org.junit.Assert.*;
 public class PointTest {
 
     @Test
-    public void when00to20then2() {
-        int expected = 2;
-        int x1 = 0;
+    public void when10to00then2() {
+        double expected = 1;
+        int x1 = 1;
         int y1 = 0;
-        int x2 = 2;
+        int x2 = 0;
         int y2 = 0;
+        double out = Point.distance(x1, y1, x2, y2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when11to00then2() {
+        double expected = 1.41;
+        int x1 = 1;
+        int y1 = 1;
+        int x2 = 0;
+        int y2 = 0;
+        double out = Point.distance(x1, y1, x2, y2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when12to34then2() {
+        double expected = 2.82;
+        int x1 = 1;
+        int y1 = 2;
+        int x2 = 3;
+        int y2 = 4;
         double out = Point.distance(x1, y1, x2, y2);
         Assert.assertEquals(expected, out, 0.01);
     }
