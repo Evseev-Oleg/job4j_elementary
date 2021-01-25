28e68fff8b321fd8d1970c9166225a920d2c742e
Evseev Oleg <oleg063053@gmail.com>
2021-01-25 11:49:31 +0400

    6.0.1.1. Размер массива [#456301]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -5,5 +5,7 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        System.out.println("Размер массива ages:" + ages.length + ". Размер массива surname:"
+        + surnames.length + ". Размер массива prices:" + prices.length + ".");
     }
 }
