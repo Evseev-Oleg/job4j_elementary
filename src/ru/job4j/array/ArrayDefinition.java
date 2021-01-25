19f7581083befd06077a3b68d83c56a7223afed3
Evseev Oleg <oleg063053@gmail.com>
2021-01-25 11:55:59 +0400

    6.0.2. Заполнение массива. [#456290]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -5,7 +5,13 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[5];
+        names[0] = "Vlad";
+        names[1] = "Oleg";
+        names[2] = "Andrey";
+        names[3] = "Petr";
+        names[4] = "Sava";
         System.out.println("Размер массива ages:" + ages.length + ". Размер массива surname:"
-        + surnames.length + ". Размер массива prices:" + prices.length + ".");
+                + surnames.length + ". Размер массива prices:" + prices.length + ".");
     }
 }
