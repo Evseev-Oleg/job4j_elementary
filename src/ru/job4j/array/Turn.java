c85857a373c39a6e852505d6f0b8a7796c57a2ac
Evseev Oleg <oleg063053@gmail.com>
2021-01-25 21:32:57 +0400

    6.4.1.1. Обход массива с последнего элемента [#456303]

--- src/ru/job4j/array/Turn.java
+++ src/ru/job4j/array/Turn.java
@@ -6,7 +6,18 @@ public class Turn {
             int temp = array[i];
             array[i] = array[array.length - 1 - i];
             array[array.length - 1 - i] = temp;
+            if (array[i] % 2 == 0) {
+                System.out.println(array[i]);
+            }
+            if (array[array.length - 1 - i] % 2 == 0) {
+                System.out.println(array[array.length - 1 - i]);
+            }
         }
         return array;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8};
+        Turn.back(array);
     }
 }
